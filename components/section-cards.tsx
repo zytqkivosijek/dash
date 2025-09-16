0cc37bedb180525f21dc871ad6959316f8fe0394
zytqkivosijek <146926728+zytqkivosijek@users.noreply.github.com>
2025-09-16 19:27:18 -0300

    Updated section-cards.tsx

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -15,7 +15,7 @@ export function SectionCards() {
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
+          <CardDescription>Saldo total disponível</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
             $1,250.00
           </CardTitle>
